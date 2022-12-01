1b17668865c909ce50b3ea98d4eee1bc58732a3b
Mikha Sitepu <mikhasitepu2467@gmail.com>
2022-12-01 12:13:20 +0700

    refactor

--- src/models/prodi.model.js
+++ src/models/prodi.model.js
@@ -1,32 +1,32 @@
 module.exports = (sequelize, Sequelize) => {
-    const Prodi = sequelize.define(
-      "prodi",
-      {
-        id: {
-          primaryKey: true,
-          type: Sequelize.INTEGER,
-          allowNull: false,
-          autoIncrement: true
-        },
-        nama: {
-          type: Sequelize.STRING,
-          allowNull: false,
-        },
-        createdAt: {
-          type: Sequelize.BIGINT,
-          defaultValue: Sequelize.literal("FLOOR(EXTRACT(EPOCH FROM NOW()))"),
-        },
-        updatedAt: {
-          type: Sequelize.BIGINT,
-          defaultValue: Sequelize.literal("FLOOR(EXTRACT(EPOCH FROM NOW()))"),
-        },
+  const Prodi = sequelize.define(
+    "prodi",
+    {
+      id: {
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
       },
-      {
-        paranoid: true,
-        freezeTableName: true,
-        underscored: true,
-        indexes: [{ unique: true, fields: ["id"]}, { fields: ["deleted_at"] }],
-      }
-    );
-    return Prodi;
-  };
+      nama: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      createdAt: {
+        type: Sequelize.BIGINT,
+        defaultValue: Sequelize.literal("FLOOR(EXTRACT(EPOCH FROM NOW()))"),
+      },
+      updatedAt: {
+        type: Sequelize.BIGINT,
+        defaultValue: Sequelize.literal("FLOOR(EXTRACT(EPOCH FROM NOW()))"),
+      },
+    },
+    {
+      paranoid: true,
+      freezeTableName: true,
+      underscored: true,
+      indexes: [{ fields: ["deleted_at"] }],
+    }
+  );
+  return Prodi;
+};
--- src/repositories/prodi.repo.js
+++ src/repositories/prodi.repo.js
@@ -1,17 +1,15 @@
 class ProdiRepository {
-    constructor(Prodi) {
-      this.prodi = Prodi;
-    }
-  
-    create = async (data) => {
-      return await this.prodi.create(data);
-    };
-  
-    getAll = async () => {
-        return await this.prodi.findAll();
-    }
-   
+  constructor(Prodi) {
+    this.prodi = Prodi;
   }
-  
-  module.exports = ProdiRepository;
-  
+
+  create = async (data) => {
+    return await this.prodi.create(data);
+  };
+
+  getAll = async () => {
+    return await this.prodi.findAll();
+  };
+}
+
+module.exports = ProdiRepository;
